7062d820d5b711595acbb8aadfc1826a623aafed
kousiclattala <kousiclattala@gmailom>
2022-08-27 18:47:50 +0530

    removed db connection

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const welcome = require("./welcome");
 
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/twiliochat", (err) => {
-  if (err) {
-    console.log("Error in connecting DB");
-  } else {
-    console.log("Connected to DB");
-  }
-});
+// mongoose.connect("mongodb://localhost:27017/twiliochat", (err) => {
+//   if (err) {
+//     console.log("Error in connecting DB");
+//   } else {
+//     console.log("Connected to DB");
+//   }
+// });
 
 app.use("/", welcome);
 app.use("/api/v1", router);
